c47b86c9fde0ae96f80fee77e397a5931ee155d8
Bayart <e_bayart2002@yahoo.com>
2025-03-31 15:00:21 -0500

    chocolate bar changed

--- hackerrank/Birthday_Chocolate_bar.java
+++ hackerrank/Birthday_Chocolate_bar.java
@@ -1,18 +1,21 @@
 public static int birthday(List<Integer> s, int d, int m) {
     // Write your code here
     int bar = 0;
+    int sum = 0;
     if (d > 31 || d < 1 || m > 12 || m < 1) {
         return bar;
     }
 
-    for (int i = 0; i < s.size(); i++) {
-        for (int j = i; j < s.size(); j++) {
-            if (s.get(i) + s.get(j) == d) {
-                bar++;
-            }
+    for (int i = 0; i < s.size() + 1 - m; i++) {
+        for (int j = 0; j < m; j++) {
+            sum = sum + s.get(i + j);
+            System.out.println(i + ", " + j + "," + sum);
         }
+
+        if (sum == d) {
+            bar++;
+        }
+        sum = 0;
     }
-
     return bar;
-
 }
