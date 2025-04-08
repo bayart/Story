4ff5e5374f1d0606eb30e7788ce9733b2a5cd7ad
Bayart <e_bayart2002@yahoo.com>
2025-04-07 20:26:07 -0500

    sockermerchant modified

--- hackerrank/sockerMerchant.java
+++ hackerrank/sockerMerchant.java
@@ -1,25 +1,28 @@
 public static int sockMerchant(int n, List<Integer> ar) {
     int pairs = 0;
-    int i = ar.size() - 1;
-    while (ar.size() >= 2) {
-        i = ar.size() - 1;
-        for (int j = i - 1; j >= 0; j--) {
-            if (ar.get(i) == ar.get(j)) {
-                System.out.println("pair = " + i + ", " + j);
+    int i = ar.size()-1;
+    while( ar.size()>=2){
+        i=ar.size()-1;
+        System.out.println("next : "+ar.get(i));
+            for(int j = i-1; j>=0; j--){
+            if(ar.get(i) == ar.get(j)){
+                System.out.println("pair = "+i+", "+j);
                 pairs++;
+                ar.remove(i);
                 ar.remove(j);
-                // ar.remove(i);
-                i = i - 2;
-                j = 0;
-
-            } else {
-                System.out.println(i + ", " + j);
+                i=i-2;
+                break;
+                
+            }else{
+                System.out.println(i+", "+j);
+            }
+            if(!ar.isEmpty() && i>=0 && j==0 && ar.get(i) != ar.get(j) ){
+                ar.remove(i);
             }
         }
-        if (!ar.isEmpty() && i >= 0) {
-            ar.remove(i);
-        }
+        
+        
         i--;
-    }
-    return pairs;
-}
+      }
+      System.out.println(pairs);
+     return pairs;
