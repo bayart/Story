704f56e4cc44eba580d2629da3efefaa0352914f
Bayart <e_bayart2002@yahoo.com>
2025-04-11 13:30:10 -0500

    counting valleys third modify

--- hackerrank/countingValleys.java
+++ hackerrank/countingValleys.java
@@ -1,6 +1,10 @@
 public static int countingValleys(int steps, String path) {
+
+    long startTime = System.nanoTime();
+
     int numOfValley = 0;
     String currentStep;
+    int valleyEnd = 0;
     int seaLevel = 0;
 
     System.out.println(path);
@@ -13,12 +17,19 @@ public static int countingValleys(int steps, String path) {
         } else {
             seaLevel--;
         }
+        if (i > 1 & seaLevel == 0 && currentStep.equals("U")) {
+            valleyEnd = 1;
+        }
 
-        if (seaLevel == 0 && currentStep.equals("D")) {
+        if (seaLevel == 0 && currentStep.equals("U") && valleyEnd == 1) {
             numOfValley++;
+            valleyEnd = 0;
         }
         System.out.println(seaLevel + ", and current :" + currentStep + "valleyCount:" + numOfValley);
 
     }
+    // long endTime = System.nanoTime();
+    // long duration = endTime - startTime;
+    // System.out.println("TIME: " + duration + " ns");
     return numOfValley;
 }
