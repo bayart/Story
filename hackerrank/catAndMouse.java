7be781c477010b534d9666eaa5574f3b099ec0ae
Bayart <e_bayart2002@yahoo.com>
2025-04-18 13:22:26 -0500

    cat and mouse position

---
+++ hackerrank/catAndMouse.java
@@ -0,0 +1,6 @@
+static String catAndMouse(int x, int y, int z) {
+
+    System.out.println("Cat A's position: " + x + ", Cat B's position: " + y + ", Mouse C's position: " + z);
+
+    return "Cat A";
+}
