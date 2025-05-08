efe202f4cabf9dcdc575d1f4b2ae5583355a7fd1
Bayart <e_bayart2002@yahoo.com>
2025-05-08 17:12:00 -0500

    modified formingMagicSquare

--- hackerrank/formingMagicSquare.java
+++ hackerrank/formingMagicSquare.java
@@ -1,5 +1,3 @@
-import java.util.List;
-
 public static int formingMagicSquare(List<List<Integer>> s) {
 
     int magicCost = 0;
@@ -7,41 +5,49 @@ public static int formingMagicSquare(List<List<Integer>> s) {
     int differerence = 0;
     int rowIndex = 0;
     int colIndex = 0;
+    int rowMax = 0;
+    int rowSum = 0;
+    // for (int i = 0; i < s.size(); i++) {
+    // for (int j = 0; j < s.size(); j++) {
+    // System.out.println(s.get(i).get(j));
+    // }
+    // System.out.println("current row :"+i);
+
+    // }
 
     for (int i = 0; i < s.size(); i++) {
-        int rowMax = s.get(i).get(0);
-        int rowSum = 0;
+        rowSum = 0;
+        rowMax = s.get(i).get(0);
+        rowIndex = i;
+        colIndex = 0;
+        System.out.println("rowmax: " + rowMax);
 
         for (int j = 0; j < s.size(); j++) {
             currentRow = i;
+
             rowSum = rowSum + s.get(i).get(j);
             if (rowMax < s.get(i).get(j)) {
                 rowMax = s.get(i).get(j);
                 rowIndex = i;
                 colIndex = j;
             }
-
         }
         if (rowSum != 15) {
             differerence = 15 - rowSum;
-            if (currentRow == 0) {
-                magicCost = magicCost + differerence;
 
-                s.get(rowIndex).set(colIndex, s.get(rowIndex).get(colIndex) + magicCost);
-                System.out.println(s);
-            }
-            {
-                System.out.println("test: " + currentRow + ", " + differerence);
-            }
+            magicCost = magicCost + differerence;
 
+            s.get(rowIndex).set(colIndex, s.get(rowIndex).get(colIndex) + differerence);
+            System.out.println(s);
+
+            // System.out.println("test: " + currentRow + ", " + differerence);
         }
 
-        // System.out.println("row sum = " + rowSum);
-        // System.out.println("rowmax = " + rowMax);
-        // System.out.println("cost = " + magicCost);
-        // System.out.println("--------------------------");
+        System.out.println("row sum = " + rowSum);
+        System.out.println("rowmax = " + rowMax);
+        System.out.println("cost = " + magicCost);
+        System.out.println("--------------------------");
     }
 
     return magicCost;
-
 }
